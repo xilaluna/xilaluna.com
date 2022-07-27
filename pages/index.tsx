28e001b9013c0bb961b75d9602a11b94706460c8
Xila Luna <xilaluna2@gmail.com>
2022-07-26 23:44:50 -0700

    [add] init pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import type { NextPage } from "next"
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <h1 className="text-3xl text-red-500 font-bold underline">Hello world!</h1>
-  )
-}
+    <h1 className="text-3xl font-bold underline text-red-500">Hello worlds!</h1>
+  );
+};
 
-export default Home
+export default Home;
---
+++ pages/projects.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+const Projects: NextPage = () => {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-500">Projects</h1>
+  );
+};
+
+export default Projects;
