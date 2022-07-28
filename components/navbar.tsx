fcff8102996f6500e2b95c74b90a9cc57c565626
Xila Luna <xilaluna2@gmail.com>
2022-07-27 19:22:12 -0700

    [add] init pages

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,39 @@
+import Link from 'next/link';
+
 const Navbar = () => {
-  return <h1>Navbar</h1>;
+  return (
+    <div>
+      <div className="p-2" />
+      <div className="flex justify-between items-center border-b border-black py-2">
+        <div className="flex-1">
+          <p>Status</p>
+        </div>
+        <div className="flex-1 flex justify-center">
+          <h1>Xila Luna</h1>
+        </div>
+
+        <ul className="flex-1 flex justify-end">
+          <li className="px-1">LinkedIn</li>
+          <li className="px-1">GitHub</li>
+          <li className="px-1">Twitter</li>
+        </ul>
+      </div>
+
+      <div className="py-2" />
+
+      <div className="flex items-center">
+        |
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        |
+        <Link href="/projects">
+          <a>Projects</a>
+        </Link>
+        |
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
---
+++ pages/skills.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+const Skills: NextPage = () => {
+  return (
+    <h1 className="text-3xl font-bold underline text-red-500">Projects</h1>
+  );
+};
+
+export default Skills;
