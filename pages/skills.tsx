d12cc269ff22636a23680958702243897ce4268f
Xila Luna <xilaluna2@gmail.com>
2022-07-29 17:39:14 -0700

    [update] index layout and skills

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -1,9 +1,7 @@
 import type { NextPage } from 'next';
 
 const Skills: NextPage = () => {
-  return (
-    <h1 className="text-3xl font-bold text-red-500 underline">Projects</h1>
-  );
+  return <h1 className="text-3xl font-bold text-red-500 underline">Skills</h1>;
 };
 
 export default Skills;
