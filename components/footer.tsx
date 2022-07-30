4196dc722059354ae752a87d6d1404969bc22e12
Xila Luna <xilaluna2@gmail.com>
2022-07-29 17:38:40 -0700

    [update] footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,19 @@
+const Copyright = () => {
+  return (
+    <p>
+      {'Copyright © '}
+      {new Date().getFullYear()}
+      {' Xila Luna'}
+    </p>
+  );
+};
+
 const Footer = () => {
-  return <h1>Footer</h1>;
+  return (
+    <div className="flex items-center justify-center border-t border-neutral-600 py-5">
+      <Copyright />
+    </div>
+  );
 };
 
 export default Footer;
