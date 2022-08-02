5ada9d12d0019621cf765a043265205459d00f03
Xila Luna <xilaluna2@gmail.com>
2022-08-01 21:12:40 -0700

    [add] homepage and project

---
+++ pages/projects/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+
+const Projects: NextPage = () => {
+  return (
+    <div className="flex flex-1 flex-col items-center justify-center">
+      <h1>Projects</h1>
+      <div className="my-5 w-12 border-b border-neutral-600" />
+    </div>
+  );
+};
+
+export default Projects;
