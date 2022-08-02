902260c1fa18c435b818eeaef1e74a64cfadf8a4
Xila Luna <xilaluna2@gmail.com>
2022-08-02 10:38:01 -0700

    [add] dark mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/layout';
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider enableSystem={true} attribute="class">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 }
 
