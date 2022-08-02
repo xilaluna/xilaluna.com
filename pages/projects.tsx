d30e45984f82ccc8b43a4aff51c6ec2d6d271443
Xila Luna <xilaluna2@gmail.com>
2022-08-01 21:12:40 -0700

    [add] homepage and project

--- pages/projects.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next';
-
-const Projects: NextPage = () => {
-  return (
-    <h1 className="text-3xl font-bold underline text-red-500">Projects</h1>
-  );
-};
-
-export default Projects;
