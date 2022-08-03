5e00a508e3b258f8544f83b82cde33c9fae4f9ce
Xila Luna <xilaluna2@gmail.com>
2022-08-03 12:38:15 -0700

    [update] nav index and global css

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -2,9 +2,16 @@ import type { NextPage } from 'next';
 
 const Projects: NextPage = () => {
   return (
-    <div className="flex flex-1 flex-col items-center justify-center">
-      <h1>Projects</h1>
-      <div className="my-5 w-12 border-b border-neutral-600" />
+    <div>
+      <div className="flex flex-col items-center justify-center">
+        <h1>Projects</h1>
+        <p>Collection of my work</p>
+        <div className="my-5 w-12 border-b border-neutral-600" />
+      </div>
+      <div className="grid grid-cols-2 gap-2">
+        <div>Hello</div>
+        <div>Hello</div>
+      </div>
     </div>
   );
 };
