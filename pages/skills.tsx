eaf8ce5b6c7e5cdb7608d82a3971f826e0b24608
Xila Luna <xilaluna2@gmail.com>
2022-08-25 19:35:44 -0700

    [update] skills page

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from 'next';
 
 const Skills: NextPage = () => {
-  return <h1 className="text-3xl font-bold text-red-500 underline">Skills</h1>;
+  return (
+    <div className="page-container">
+      <div className="flex flex-col items-center justify-center space-y-5">
+        <h1 className="main-heading">Skills</h1>
+        <p className="text-center">A list of my skills</p>
+      </div>
+      <div className="border-divider"></div>
+    </div>
+  );
 };
 
 export default Skills;
