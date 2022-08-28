06bd837a852012e95201b3ff8c0e700487720c30
Xila Luna <xilaluna2@gmail.com>
2022-08-28 14:25:39 -0700

    [add] head to layout

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,10 +1,33 @@
 import { ReactNode } from 'react';
+import Head from 'next/head';
 import Navbar from './navbar';
 import Footer from './footer';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <>
+      <Head>
+        <title>Xila Luna</title>
+        <meta name="description" content="Xila Luna's Portfolio" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <Navbar />
       {children}
       <Footer />
