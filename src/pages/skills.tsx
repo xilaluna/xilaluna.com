35f79bd595df9c6e3c24bc956ec755c460daebc7
Xila Luna <xilaluna2@gmail.com>
2022-09-05 22:35:45 -0700

    [update] integrate absoulte paths

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -11,7 +11,7 @@ import {
 } from 'chart.js';
 import { Bar } from 'react-chartjs-2';
 import { useTheme } from 'next-themes';
-import skillsData from '../../data/skills.json';
+import skillsData from '../../public/data/skills.json';
 
 ChartJS.register(
   CategoryScale,
