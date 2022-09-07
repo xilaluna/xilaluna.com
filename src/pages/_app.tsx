71b7cf1d3edfd6e7977b304299e85379115633a5
Xila Luna <xilaluna2@gmail.com>
2022-09-07 12:49:25 -0700

    [rollback]

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,42 +1,8 @@
-import type { AppProps } from 'next/app';
-import { ThemeProvider } from 'next-themes';
-import Head from 'next/head';
-import '../styles/globals.css';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ThemeProvider enableSystem={true} attribute="class">
-      <Head>
-        <title>Xila Luna</title>
-        <meta name="description" content="Xila Luna's Portfolio" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-      </Head>
-      <div className="flex min-h-screen flex-col">
-        <Navbar />
-        <Component {...pageProps} />
-        <Footer />
-      </div>
-    </ThemeProvider>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
