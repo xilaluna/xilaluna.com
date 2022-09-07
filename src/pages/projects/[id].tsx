b5778bf61de87ffaec50f85d1d5ab867b9d969aa
Xila Luna <xilaluna2@gmail.com>
2022-09-07 00:01:32 -0700

    [update] image, hero btns and project page

--- src/pages/projects/[id].tsx
+++ src/pages/projects/[id].tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import path from 'path';
 import fs from 'fs';
-import { CaretRight, ArrowUpRight } from 'phosphor-react';
+import { CaretLeft, ArrowSquareOut } from 'phosphor-react';
 
 interface linkInterface {
   name: string;
@@ -54,30 +54,35 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 const Project = (props: { project: projectInterface }) => {
   return (
-    <div className="container space-y-5">
-      <div className="flex items-end justify-start space-x-1">
-        <Link href={'/projects'}>
-          <a className="link-style pb-px">Projects</a>
-        </Link>
-
-        <CaretRight className="mb-1 inline-block align-middle" />
-        <h1 className="text-2xl text-neutral-900 dark:text-neutral-50">
+    <main className="secondary-page">
+      <div className="flex flex-col items-center justify-center space-y-2 sm:space-y-5">
+        <h1 className="heading-color text-3xl font-semibold sm:text-4xl">
           {props.project.title}
         </h1>
       </div>
+      <section className="flex flex-col space-y-2">
+        <Link href={'/projects'}>
+          <a className="link-style">
+            <CaretLeft className="mb-0.5 mr-1 inline-block align-middle" />
+            Back to Projects
+          </a>
+        </Link>
+        <p className="text-justify">{props.project.description}</p>
 
-      <p className="text-justify">{props.project.description}</p>
+        <ul className="list-inside list-disc	">
+          {props.project.links.map((link: linkInterface) => {
+            return (
+              <li key={link.name}>
+                <a href={link.link} className="link-style">
+                  {link.name}{' '}
+                  <ArrowSquareOut className="mb-1 inline-block align-middle" />
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
 
-      <div className="flex space-x-2">
-        {props.project.links.map((link: linkInterface) => {
-          return (
-            <a href={link.link} key={link.name} className="btn">
-              {link.name}{' '}
-              <ArrowUpRight className="mb-1 inline-block align-middle" />
-            </a>
-          );
-        })}
-      </div>
       {props.project.images.map((image: string) => {
         return (
           <div className="aspect-w-16 aspect-h-9 w-full" key={image}>
@@ -91,7 +96,7 @@ const Project = (props: { project: projectInterface }) => {
           </div>
         );
       })}
-    </div>
+    </main>
   );
 };
 
