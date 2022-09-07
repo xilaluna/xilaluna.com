9664e42c4a1b04a5cd252207326730e6d4366288
Xila Luna <xilaluna2@gmail.com>
2022-09-07 13:51:45 -0700

    [add] dmmono

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      dmmono: ["DM Mono", "monospace"],
+    },
   },
   plugins: [],
 }
