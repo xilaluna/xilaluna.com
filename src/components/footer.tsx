35a180899bc1c6189d49bdacc627d0a3e4b2811d
Xila Luna <xilaluna2@gmail.com>
2022-09-06 18:19:01 -0700

    [update] position

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 const Copyright = () => {
   return (
-    <p className="text-neutral-400 dark:text-neutral-600">
+    <p className="text-center text-neutral-400 dark:text-neutral-600">
       {'Copyright © '}
       {new Date().getFullYear()}
       {' Xila Luna'}
@@ -10,8 +10,10 @@ const Copyright = () => {
 
 const Footer: React.FC = () => {
   return (
-    <footer className="border-color mx-auto flex max-w-screen-sm items-center justify-center border-t py-3 sm:py-5">
-      <Copyright />
+    <footer className="absolute bottom-0 w-full">
+      <div className="border-color mx-auto max-w-screen-sm border-t py-3 sm:py-5">
+        <Copyright />
+      </div>
     </footer>
   );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,40 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import Head from 'next/head';
 import '@/styles/globals.css';
-import Layout from '@/components/Layout';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
-      <Layout>
+      <Head>
+        <title>Xila Luna</title>
+        <meta name="description" content="Xila Luna's Portfolio" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <div className="relative min-h-screen">
+        <Navbar />
         <Component {...pageProps} />
-      </Layout>
+        <Footer />
+      </div>
     </ThemeProvider>
   );
 }
