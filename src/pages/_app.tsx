f4fc55e20f2c24dffc35315debe85c0627fbf32b
Xila Luna <xilaluna2@gmail.com>
2022-09-07 14:24:22 -0700

    [add] styles and dark theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,44 @@
-import "@/styles/globals.css"
-import type { AppProps } from "next/app"
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
+import Head from 'next/head';
+
+import '@/styles/globals.css';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider enableSystem={true} attribute="class">
+      <Head>
+        <title>Xila Luna</title>
+        <meta name="description" content="Xila Luna's Portfolio" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+
+      <div className="flex min-h-screen flex-col">
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
