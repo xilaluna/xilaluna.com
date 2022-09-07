0ab8c9ff3fe78d240c4533169b5c1f84b69961da
Xila Luna <xilaluna2@gmail.com>
2022-09-07 00:26:02 -0700

    [update] im tired and want this shit to deploy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
-import Hero from '~/components/Hero';
-import Timeline from '~/components/Timeline';
-import GetInTouch from '~/components/GetInTouch';
+import Hero from '../components/Hero';
+import Timeline from '../components/Timeline';
+import GetInTouch from '../components/GetInTouch';
 
 const Home: NextPage = () => {
   return (
