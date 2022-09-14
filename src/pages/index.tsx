cde79a0d12b46c43571e91316977c45172a9e7a4
Xila Luna <xilaluna2@gmail.com>
2022-09-13 17:51:56 -0700

    :feature add for index and projects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,40 @@
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import Hero from '@/components/Hero';
 import Timeline from '@/components/Timeline';
 import GetInTouch from '@/components/GetInTouch';
 
 const Home: NextPage = () => {
   return (
-    <main className="container">
-      <Hero />
-      <Timeline />
-      <GetInTouch />
-    </main>
+    <>
+      <Head>
+        <title>Xila Luna</title>
+        <meta name="description" content="Xila Luna's Portfolio" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <main className="container">
+        <Hero />
+        <Timeline />
+        <GetInTouch />
+      </main>
+    </>
   );
 };
 
