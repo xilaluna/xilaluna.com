4c3d66da407df7853d3c56aad2548b139b5f3f20
Xila Luna <xilaluna2@gmail.com>
2022-09-19 02:50:50 -0700

    refactor(heading comp): added type interface for comp

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,4 +1,9 @@
-const Heading = ({ title, subtitle }: { title: string; subtitle: string }) => {
+interface HeadingProps {
+  title: string;
+  subtitle: string;
+}
+
+const Heading: React.FC<HeadingProps> = ({ title, subtitle }) => {
   return (
     <div className="flex flex-col items-center justify-center space-y-2 sm:space-y-5">
       <h1 className="main-heading">{title}</h1>
