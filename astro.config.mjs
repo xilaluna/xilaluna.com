9e0d10d1c096f71b568d515b4f41c0310e8b7d39
xilaluna <xilaluna2@gmail.com>
2023-03-15 14:03:08 -0700

    update:format on mjs files & add to links page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    config: { applyBaseStyles: false },
-  })]
+  integrations: [
+    tailwind({
+      // used for accessing base.css
+      config: { applyBaseStyles: false },
+    }),
+  ],
 });
