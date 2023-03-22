39de082c1bc9cbc74ee9d6aebe63f1f0e7268860
xilaluna <xilaluna2@gmail.com>
2023-03-21 20:58:05 -0700

    add: color vars

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,40 @@
 /** @type {import('tailwindcss').Config} */
+
+// add opactity to rgba colors
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+}
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   fontFamily: {
     mono: ["IBM Plex Mono", "monospace"],
   },
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        skin: {
+          base: withOpacity("--color-text-base"),
+          accent: withOpacity("--color-accent"),
+          muted: withOpacity("--color-muted"),
+        },
+      },
+      backgroundColor: {
+        skin: {
+          fill: withOpacity("--color-fill"),
+        },
+      },
+      borderColor: {
+        skin: {
+          base: withOpacity("--color-muted"),
+        },
+      },
+    },
   },
   plugins: [],
 };
